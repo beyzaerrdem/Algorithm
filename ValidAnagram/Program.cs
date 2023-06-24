61938c7cbd10717efce25fc31e7076d9a69c7d65
beyzaerrdem <66781013+beyzaerrdem@users.noreply.github.com>
2023-06-24 17:26:36 +0300

    Add project files.

---
+++ ValidAnagram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValidAnagram
+{
+    //LeetCode Link:https://leetcode.com/problems/valid-anagram/
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnagram("anagram", "nagaram"));
+        }
+
+        public static bool IsAnagram(string s, string t)
+        {
+            List<char> sChar = new List<char> { };
+            List<char> tChar = new List<char> { };
+            for (int i = 0; i < s.Length; i++)
+            {
+                sChar.Add(s[i]);
+            }
+            for (int j = 0; j < t.Length; j++)
+            {
+                tChar.Add(t[j]);
+            }
+            sChar.Sort();
+            tChar.Sort();
+            return sChar.SequenceEqual(tChar);
+        }
+    }
+}
---
+++ ValidPalindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ValidPalindrome
+{
+    //https://leetcode.com/problems/valid-palindrome/solutions/
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            string pattern = @"[\d\p{P}]";
+            s = Regex.Replace(s, pattern, " ");
+            string s1 = s.Replace(" ", "");
+
+            string reverseS = "";
+
+            for (int i = s1.Length - 1; i >= 0; i--)
+            {
+                reverseS += s1[i];
+            }
+            if (reverseS.ToLower() == s1.ToLower())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
