8e96590eecd84e8cf4787ff63e04a0258fc03343
beyzaerrdem <66781013+beyzaerrdem@users.noreply.github.com>
2023-08-31 01:01:44 +0300

    MoveZeroes

---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MoveZeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 0, 1, 0, 3, 12 };
+            Console.WriteLine(MoveZeroes(num));
+        }
+
+        public static int[] MoveZeroes(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {              
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        nums[j - 1] = nums[j];
+                    }
+                    nums[nums.Length - 1] = 0;
+                }
+            }
+            return nums;
+        }
+    }
+}
